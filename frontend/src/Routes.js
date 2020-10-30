d2c22df7f8df66e2a84bb822a6d63e560945445e
sprkzoff <sprkzoff@gmail.com>
2020-10-30 08:18:08 +0700

    add demoPost route

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,6 +2,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { CookiesProvider } from 'react-cookie';
 import Login from './Login';
+import Post from './Post';
 
 function Routes() {
     return (
@@ -11,6 +12,7 @@ function Routes() {
                     <Switch>
                         <Route exact path="/" component={App} />
                         <Route exact path="/login" component={Login} />
+                        <Route exact path="/demoPost" component={Post} />
                     </Switch>
                 </BrowserRouter>
             </CookiesProvider>
