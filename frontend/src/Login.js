5ce4c577158a035c4f3dea082ec01ad300f93234
tongplw <bangkok.tong@gmail.com>
2020-10-31 17:44:24 +0700

    added login page

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,10 +1,54 @@
+import axios from 'axios';
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import "./Login.css";
 
-function Login() {
+export default function Login() {
+  const [error, setError] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const LOGIN_ERROR_MSG = 'username/password is invalid';
+
+  function validateForm() {
+    return username.length > 0 && password.length > 0;
+  }
+
+  function onClickSubmit(event) {
+    event.preventDefault();
+    try {
+      const result = axios.post('/api/login', { username, password });
+      if (result.status === 200) window.location.href = '/blog';
+      else setError(LOGIN_ERROR_MSG);
+    } catch(e) {
+      setError(LOGIN_ERROR_MSG);
+    }
+  }
+
   return (
-    <div>
-        Login
+    <div className="Login">
+      <form onSubmit={onClickSubmit}>
+        <FormGroup controlId="username" bsSize="large">
+          <FormLabel>Username</FormLabel>
+          <FormControl
+            autoFocus
+            type="username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <FormLabel>Password</FormLabel>
+          <FormControl
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </form>
     </div>
   );
 }
-
-export default Login;
