0b48b37059d7566da19869b83d49a0e01374bd72
Phatcharapon Jumruspun <darkenstardragon@Phatcharapons-MacBook-Pro.local>
2020-10-31 14:38:46 +0700

    Jwt authentication

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,9 +3,21 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { EasyconfigModule } from 'nestjs-easyconfig';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy]
+    imports: [
+        EasyconfigModule.register({ path: '.env' }),
+        UsersModule,
+        PassportModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: '60s' },
+        }),
+    ],
+    providers: [AuthService, LocalStrategy],
+    exports: [AuthService],
 })
-export class AuthModule {}
+
+export class AuthModule { }
