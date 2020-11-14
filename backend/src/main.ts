a317cc2d16ffb78f33921243f3f3d7f4f88e70ff
Tanakorn Tampanya <tanakorn.tpy@gmail.com>
2020-11-15 02:34:22 +0700

    add specific origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ALL } from 'dns';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const ALLOW_ORIGIN = ["http://localhost:3000","http://compsecproject_frontend_1:3001","http://compsecproject_frontend_1"]
+
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin:ALLOW_ORIGIN
+  })
   require('dotenv').config({ path: __dirname + '/.env'})
   await app.listen(process.env.BACKEND_SERVICE_PORT);
 }
