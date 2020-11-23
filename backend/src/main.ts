f70b12d7eb25ac886f321e9f6799d391a369ae36
tongplw <bangkok.tong@gmail.com>
2020-11-23 17:07:36 +0700

    tongplw

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ALL } from 'dns';
 import { AppModule } from './app.module';
+import { UsersController } from './users/users.controller'
 
 async function bootstrap() {
-  const ALLOW_ORIGIN = ["http://localhost:3000","http://compsecproject_frontend_1:3001","http://compsecproject_frontend_1"]
+  const ALLOW_ORIGIN = ["http://35.185.182.75:3000","http://compsecproject_frontend_1:3001","http://compsecproject_frontend_1"]
 
   const app = await NestFactory.create(AppModule);
   app.enableCors({
