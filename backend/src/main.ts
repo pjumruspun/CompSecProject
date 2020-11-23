dbcac62e69d43c5fc1d90eed56f7e63f79481a91
tongplw <bangkok.tong@gmail.com>
2020-11-23 20:59:10 +0700

    update allow origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { UsersController } from './users/users.controller'
 
 async function bootstrap() {
-  const ALLOW_ORIGIN = ["*",, "http://localhost:3000","http://compsecproject_frontend_1:3001","http://compsecproject_frontend_1"]
+  const ALLOW_ORIGIN = ["*:3000",, "http://localhost:3000","http://compsecproject_frontend_1:3001","http://compsecproject_frontend_1"]
 
   const app = await NestFactory.create(AppModule);
   app.enableCors({
